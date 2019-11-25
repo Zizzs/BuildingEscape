0b31ab5161ad6f597c693f4799b0104c47cd6cea
Alex Williams <Zizzs17@gmail.com>
2019-11-25 15:57:43 -0800

    Retrieving data from UE4 Object in PositionReport.cpp

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -13,22 +13,18 @@ UPositionReport::UPositionReport()
 	// ...
 }
 
-
 // Called when the game starts
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *ObjectName);
 }
 
-
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
 }
-
